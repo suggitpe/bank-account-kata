2e26ff043bdc60119b6512efa691e640839cb333
Peter Suggitt <me@suggs.org.uk>
2019-03-26 07:19:22 +0000

    update java test so its disabled

--- java-kata/src/test/java/org/xpdojo/bank/WithAnAccountWeCan.java
+++ java-kata/src/test/java/org/xpdojo/bank/WithAnAccountWeCan.java
@@ -1,5 +1,6 @@
 package org.xpdojo.bank;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,6 +8,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class WithAnAccountWeCan {
 
     @Test
+    @Disabled
     public void depositAnAmountToIncreaseTheBalance() {
         assertThat("no implementation you need to write the test first").isEqualTo("an implemented test");
     }
